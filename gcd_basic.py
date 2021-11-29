ce4de5a340e634fb54bc0cde718d2108239de907
arnobsarkar27 <arnob.cars@gmail.com>
2021-11-29 23:14:48 +0530

    Added some DSA files

--- gcd_basic.py
+++ gcd_basic.py
@@ -1,3 +1,25 @@
 #Learning DSA using Python
+#Basic Greatest Common Divisor Algorithm
 
-import numpy as np
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+
+num1_factors = []
+num2_factors = []
+common_factors = []
+
+num1_factors = [i for i in range(1,num1+1) if num1%i==0]
+num2_factors = [i for i in range(1,num2+1) if num2%i==0]
+
+print("Factors:")
+print("{}: {}".format(num1,num1_factors))
+print("{}: {}".format(num2,num2_factors))
+
+for i in range(len(num1_factors)):
+    for j in range(len(num2_factors)):
+        if num1_factors[i] == num2_factors[j]:
+            common_factors.append(num1_factors[i])
+            break
+
+print("List of common factors: ", common_factors)
+print("Highest Common Factor: ",common_factors[-1])
---
+++ gcd_min.py
@@ -0,0 +1,14 @@
+#A much shorter program algorithm for GCD of three numbers
+# This uses minimum of the three numbers.
+
+def gcd(num1,num2,num3):
+    common_factors = []
+    common_factors = [i for i in range(1,min(num1,num2,num3)+1) if num1%i==0 and num2%i==0 and num3%i==0]
+
+    return(common_factors[-1])
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+print("Hightst common factor: ",gcd(num1,num2,num3))
